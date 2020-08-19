d115ff02b743c43b8c42e41743eb05ee25b0e6e4
Roland Haas <rhaas@illinois.edu>
2020-08-19 11:55:06 -0500

    add test

---
+++ test/test_config.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "er.h"
+#include "er_util.h"
+
+#include "kvtree.h"
+#include "kvtree_util.h"
+
+int
+main(int argc, char *argv[]) {
+    int rc;
+    char *conf_file = NULL;
+    kvtree* er_config_values = kvtree_new();
+
+    int old_er_debug = er_debug;
+    int old_er_set_size = er_set_size;
+    int old_er_mpi_buf_size = er_mpi_buf_size;
+
+    MPI_Init(&argc, &argv);
+
+    rc = ER_Init(conf_file);
+    if (rc != ER_SUCCESS) {
+        printf("ER_Init() failed (error %d)\n", rc);
+        return rc;
+    }
+
+    /* check ER configuration settings */
+    rc = kvtree_util_set_int(er_config_values, ER_KEY_CONFIG_DEBUG,
+                             !old_er_debug);
+    if (rc != KVTREE_SUCCESS) {
+        printf("kvtree_util_set_int failed (error %d)\n", rc);
+        return rc;
+    }
+    rc = kvtree_util_set_int(er_config_values, ER_KEY_CONFIG_SET_SIZE,
+                             old_er_set_size + 1);
+    if (rc != KVTREE_SUCCESS) {
+        printf("kvtree_util_set_int failed (error %d)\n", rc);
+        return rc;
+    }
+    rc = kvtree_util_set_int(er_config_values, ER_KEY_CONFIG_MPI_BUF_SIZE,
+                             old_er_mpi_buf_size + 1);
+    if (rc != KVTREE_SUCCESS) {
+        printf("kvtree_util_set_int failed (error %d)\n", rc);
+        return rc;
+    }
+
+    printf("Configuring ER...\n");
+    rc = ER_Config(er_config_values);
+    if (rc != ER_SUCCESS) {
+        printf("ER_Config() failed (error %d)\n", rc);
+        return rc;
+    }
+
+    printf("Configuring ER a second time (this should fail)...\n");
+    rc = ER_Config(er_config_values);
+    if (rc == ER_SUCCESS) {
+        printf("ER_Config() succeeded unexpectedly (error %d)\n", rc);
+        return rc;
+    }
+
+    if (er_debug != !old_er_debug) {
+        printf("ER_Config() failed to set %s: %d != %d\n",
+               ER_KEY_CONFIG_DEBUG, er_debug, !old_er_debug); 
+        return EXIT_FAILURE;
+    }
+
+    if (er_set_size != old_er_set_size + 1) {
+        printf("ER_Config() failed to set %s: %d != %d\n",
+               ER_KEY_CONFIG_SET_SIZE, er_set_size, old_er_set_size); 
+        return EXIT_FAILURE;
+    }
+
+    if (er_mpi_buf_size != old_er_mpi_buf_size + 1) {
+        printf("ER_Config() failed to set %s: %d != %d\n",
+               ER_KEY_CONFIG_MPI_BUF_SIZE, er_mpi_buf_size,
+               old_er_mpi_buf_size); 
+        return EXIT_FAILURE;
+    }
+
+    rc = ER_Finalize();
+    if (rc != ER_SUCCESS) {
+        printf("ER_Finalize() failed (error %d)\n", rc);
+        return rc;
+    }
+
+    MPI_Finalize();
+
+    return ER_SUCCESS;
+}
