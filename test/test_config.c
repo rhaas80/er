8d13068b54a4f4957cceeb8533868fcaee06fe7b
Roland Haas <rhaas@illinois.edu>
2020-09-30 19:48:19 -0500

    test_config: adapt to ER being configurable more than once

--- test/test_config.c
+++ test/test_config.c
@@ -44,17 +44,37 @@ main(int argc, char *argv[]) {
         return rc;
     }
 
-    printf("Configuring ER...\n");
+    printf("Configuring ER (first set of options)...\n");
     if (ER_Config(er_config_values) == NULL) {
         printf("ER_Config() failed\n");
         return EXIT_FAILURE;
     }
 
-    printf("Configuring ER a second time (this should fail)...\n");
-    if (ER_Config(er_config_values) != NULL) {
-        printf("ER_Config() succeeded unexpectedly\n");
+    /* check options we just set */
+
+    if (er_debug != !old_er_debug) {
+        printf("ER_Config() failed to set %s: %d != %d\n",
+               ER_KEY_CONFIG_DEBUG, er_debug, !old_er_debug);
         return EXIT_FAILURE;
     }
+
+    if (er_set_size != old_er_set_size + 1) {
+        printf("ER_Config() failed to set %s: %d != %d\n",
+               ER_KEY_CONFIG_SET_SIZE, er_set_size, old_er_set_size);
+        return EXIT_FAILURE;
+    }
+
+    /* configure remainder of options */
+    kvtree_delete(&er_config_values);
+    er_config_values = kvtree_new();
+
+    printf("Configuring ER (second set of options)...\n");
+    if (ER_Config(er_config_values) == NULL) {
+        printf("ER_Config() failed\n");
+        return EXIT_FAILURE;
+    }
+
+    /* check all values once more */
 
     if (er_debug != !old_er_debug) {
         printf("ER_Config() failed to set %s: %d != %d\n",
